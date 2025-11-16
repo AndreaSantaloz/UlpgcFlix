baca0103d9b36899b0d2665c2b40c75fb8961e33
AndreaSantaloz <andreasantaloz@gmail.com>
2025-11-16 22:29:36 +0000

    añadir  en greeting para ver en emulador fuera de preview y quitar librerias de comienzo.kt

--- app/src/main/java/com/example/ulpgcflix/MainActivity.kt
+++ app/src/main/java/com/example/ulpgcflix/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,10 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UlpgcFlixTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Greeting()
                 }
             }
         }
@@ -31,11 +26,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    UlpgcFlixTheme {
+        NavigationGraph()
+    }
 }
 
 @Preview(showBackground = true)
